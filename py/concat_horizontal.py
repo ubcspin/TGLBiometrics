829e60a45da721c45cf89108536030d9ddfcc507
pbucci <pbucci@gmail.com>
2016-06-21 16:43:34 -0700

    Update concat_horizontal.py

--- py/concat_horizontal.py
+++ py/concat_horizontal.py
@@ -68,7 +68,7 @@ def main():
 				out[i] = out[i] + "," + f[i]
 
 		# unique file for csv
-		outfile = open(directory + "concat_h_" + filenames[0] + "." + "csv",'w+')
+		outfile = open(directory + "concat_h_" + filenames[0].split('/')[-1].split('.')[0] + "." + "csv",'w+')
 
 		# write out all lines
 		for line in out:
